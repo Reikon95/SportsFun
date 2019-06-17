d057de773ff7fca0ca77d6f3cde7cd9a8782e4b1
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-06-17 18:32:02 +0100

    Update Records.py
    added draw/tie functionality

--- Records.py
+++ Records.py
@@ -1,5 +1,12 @@
-#Calculate a team's win percentage
+#Calculate a team's win percentage, can be adapted not to include draws
 
-def win_percentage(wins, losses):
-  return wins / (wins + losses) * 100
+def win_percentage(wins, losses, draws):
+  return wins / (wins + losses + draws) * 100
 print(win_percentage(5, 5))
+
+#Calculate a team's loss percentage
+
+def loss_percentage(wins, losses, draws):
+  return losses / (wins + losses + draws) * 100
+print(loss_percentage(5, 5))
+
