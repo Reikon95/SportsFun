304ce4d02d7b0ff740895de269828e8ae575b8d9
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-06-18 09:49:05 +0100

    Update Records.py

--- Records.py
+++ Records.py
@@ -10,3 +10,7 @@ def loss_percentage(wins, losses, draws):
   return losses / (wins + losses + draws) * 100
 print(loss_percentage(5, 5))
 
+#Player goalscoring records(player, goals, games):
+  Gpg = goals/games
+  print(player + " has scored at a rate of " + str(Gpg) + " goals per game."
+        
