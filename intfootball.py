e4fb5a354006259df81014818a64edd082f3955b
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-22 17:55:28 +0100

    Update intfootball.py

--- intfootball.py
+++ intfootball.py
@@ -5,3 +5,13 @@ with open('results.csv') as results:
   away = []
   goals_home = []
   goals_away = []
+  
+  def total_goals:
+    
+  def total_wins:
+  
+  def total_draws:
+   
+  def total_losses:
+  
+  def win_percentage:
