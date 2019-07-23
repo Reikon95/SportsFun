633971e04736498bf9d1ef281092d859aca6b3e6
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-23 16:43:02 +0100

    Update intfootball.py

--- intfootball.py
+++ intfootball.py
@@ -1,17 +1,21 @@
 import math
 
-with open('results.csv') as results:
-  home = []
-  away = []
-  goals_home = []
-  goals_away = []
+with open('Int_Football.csv') as results:
   
-  def total_goals:
+  def total_matches_played():
+    i = 0
+    for elem in results:
+      i += 1
+    return 'The total number of international football matches played as of July 2019 is ' + str(i)
+
+  
+  print(total_matches_played())
+  
+  def get_nation(country):
+    i = 0
+    for elem in results:
+      if elem == country:
+        i += 1
     
-  def total_wins:
-  
-  def total_draws:
-   
-  def total_losses:
-  
-  def win_percentage:
+    return country + ' has played a total of '+ str(i) + ' international matches so far.'
+  print(get_nation('Gabon'))
