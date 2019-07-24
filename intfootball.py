9b0682e22fc49ef87d5e6791d406d33beb55675f
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-24 12:23:11 +0100

    Update intfootball.py

--- intfootball.py
+++ intfootball.py
@@ -10,7 +10,13 @@ with open('Int_Football.csv') as results:
     i = 0
     for elem in results:
       i += 1
-    return 'The total number of international football matches played as of July 2019 is ' + str(i)
+    return 'The total number of international football matches played as of July 2019 is ' + str(i) + '. That works out to ' + str(int((i / (2019-1871)))) + ' per year on average.'
+
+  def num_matches():
+    i = 0
+    for elem in results:
+      i += 1
+    return i
 
   
   print(total_matches_played())
@@ -25,13 +31,12 @@ with open('Int_Football.csv') as results:
   
   # print(get_nation('Scotland'))
 
-  # def nation ():
-  #   i = 0
-  #   for line in results.readlines():
-  #     if '1883-03-10,England,Scotland,2,3,Friendly,Sheffield,England,FALSE' in line:
-  #       i += 1
-  #     else:
-  #       continue 
-  #   return i
-  # print(nation())
-
+  def split_em():
+    i = 0
+    home = []
+    while i < 40:
+      split_res = match.split(",")
+      home.append(split_res[1])
+      i += 1
+    return home 
+  print(split_em())
