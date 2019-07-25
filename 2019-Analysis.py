d10fba27aafbc02c7bfbde570341166d0afb2c20
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-25 15:52:21 +0100

    Update 2019-Analysis.py

--- 2019-Analysis.py
+++ 2019-Analysis.py
@@ -1,4 +1,3 @@
-
 with open('2019results.csv') as lines_doc:
   # for line in lines_doc.readlines():
     # print(line)
@@ -8,19 +7,20 @@ with open('2019results.csv') as lines_doc:
     for line in lines_doc.readlines():
       if country in line:
         i += 1
-        print(i)
-    print(i)
+    
     if i > 0:
-      return 'Yes, ' + country + ' has played ' + str(i) + ' times this year.'
+      print('Yes, ' + country + ' has played ' + str(i) + ' times this year.')
+      return True
     else:
-      return 'Nope. Either that is not a country or they have not played yet'
+      print('Nope. Either that is not a country or they have not played yet')
+      return False
   
   print(is_country_present('Scotland'))
   
 
-    def how_many_wins(country):
-      if 'Yes' in is_counry_present(country):
-        return country ' has won ' + str(x) ' out of ' str(x) + ' games.' 
-      else:
-        return 
-    how_many_wins('scotland')
+  def how_many_wins(country):
+    if (1 + 1) == 2:
+      return is_country_present(country)
+    else:
+      return 'something has gone horribly wrong here'
+  print(how_many_wins('Scotland'))
